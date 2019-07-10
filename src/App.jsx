2a68edbd3e18f68aa851612a2af8723dc085a588
Tariq <tic084@gmail.com>
2019-07-10 01:26:25 -0400

    add holly hedge image

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,23 @@ import { withStyles } from '@material-ui/core';
 
 const styles = {
   root: {
-    background: 'url(img/pink_rice.png)',
+    fontFamily: "'Quattrocento', serif",
     display: 'flex',
     flexDirection: 'column',
     textAlign: 'center',
     alignItems: 'center',
     height: '100%'
+  },
+  img: {
+    background: 'url(img/SLIDER-holly-hedge-wedding-country-romantic-fieldstone-barn-fountain-flowers-nighttime-beautiful1.jpg)',
+    height: '250px',
+    backgroundSize: 'cover'
+  },
+  link: {
+    position: 'absolute',
+    bottom: 5,
+    right: 5,
+    color: '#fff'
   }
 };
 
@@ -23,7 +34,13 @@ class App extends PureComponent {
     return (
       <div className={classes.root}>
         <Header />
-        <Events />
+        <div>
+          <div style={{ height: '100%', marginTop: 20, position: 'relative' }}>
+            <div alt="Holly Hedge" className={classes.img} />
+            <a href="https://www.hollyhedge.com/" className={classes.link}>Holly Hedge Estate</a>
+          </div>
+          <Events />
+        </div>
       </div>
     );
   }
@@ -39,7 +56,7 @@ export default withStyles(styles)(App);
 // }
 
 // const mapDispatchToProps = (dispatch, ownProps) => bindActionCreators({
-    
+
 // }, dispatch);
 
 // export default connect(
