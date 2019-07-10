32e8779660e84e741ae790fed541c6786cdeb4dc
Tariq <tic084@gmail.com>
2019-07-09 23:59:26 -0400

    add date to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,23 @@ import { withStyles } from '@material-ui/core';
 const styles = {
   root: {
     fontFamily: "'Quattrocento', serif",
-    background: 'url(img/sand-background.png)',
+    background: 'url(img/pink_rice.png)',
     backgroundSize: '200px auto',
     borderBottom: '1px solid #000000',
     textTransform: 'uppercase',
     letterSpacing: 4,
     position: 'sticky',
-    top: 0
+    top: 0,
+    height: 110
+  },
+  title: {
+    margin: '5px 0'
+  },
+  subTitle: {
+    margin: '5px 0'
+  },
+  cursive: {
+    fontFamily: "'Cedarville Cursive', cursive"
   }
 };
 
@@ -19,7 +29,8 @@ class Header extends PureComponent {
     const { classes } = this.props;
     return (
       <div className={classes.root}>
-        <h1>Tariq &amp; Irina</h1>
+        <h1 className={classes.title}>Tariq <span className={classes.cursive}>&amp;</span> Irina</h1>
+        <h4 className={classes.subTitle}>September 4 2020</h4>
       </div>
     );
   }
