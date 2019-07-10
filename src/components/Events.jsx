3ba5ac696dbda79b6092474ba3cd318a5cfcc1aa
Tariq <tic084@gmail.com>
2019-07-10 11:26:37 -0400

    glass background for each event

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -4,14 +4,28 @@ import { bindActionCreators } from 'redux';
 import { withStyles } from '@material-ui/core';
 import { getEventsAction } from '../actions';
 
-export const styles = {
+const styles = {
   root: {
     fontFamily: "'Quattrocento', serif",
   },
   event: {
     display: 'flex',
-    margin: '40px 0',
-    textAlign: 'left'
+    margin: '20px 0',
+    padding: 10,
+    textAlign: 'left',
+    overflow: 'hidden',
+    position: 'relative',
+
+    '&:before': {
+      content: '""',
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      boxShadow: 'inset 0 0 0 200px rgba(255,255,255,0.3)',
+      filter: 'blur(45px)'
+    }
   },
   eventLeft: {
     flex: '0 0 250px'
