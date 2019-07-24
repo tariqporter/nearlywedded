50551dc7616ce42689a98350b9b102dd3cd30adf
Tariq <tic084@gmail.com>
2019-07-24 17:13:13 -0400

    add daysUntilWedding to redux

--- src/App.jsx
+++ src/App.jsx
@@ -44,11 +44,7 @@ class App extends PureComponent {
   }
 
   render() {
-    const { classes } = this.props;
-    const date1 = new Date('9/4/2020');
-    const date2 = new Date();
-    const diffTime = Math.abs(date2.getTime() - date1.getTime());
-    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+    const { classes, daysUntilWedding } = this.props;
     return (
       <div className={classes.root}>
         <Header />
@@ -58,7 +54,7 @@ class App extends PureComponent {
             <a href="https://www.hollyhedge.com/" target="_blank" rel="noopener noreferrer" className={classes.link}>Holly Hedge Estate</a>
           </div>
           <Events />
-          <div style={{ fontWeight: 'bold' }}>See you in { diffDays } days!</div>
+          <div style={{ fontWeight: 'bold' }}>See you in { daysUntilWedding } days!</div>
         </div>
       </div>
     );
@@ -66,9 +62,10 @@ class App extends PureComponent {
 }
 
 const mapStateToProps = (state) => {
-  const { events } = state;
+  const { events, daysUntilWedding } = state;
   return {
-    events
+    events,
+    daysUntilWedding
   }
 }
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,16 @@
 import { ACTION } from "../actions";
 
+const date1 = new Date('9/4/2020');
+const date2 = new Date();
+const diffTime = Math.abs(date1.getTime() - date2.getTime());
+const daysUntilWedding = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
 const initialState = {
   user: {
     id: null
   },
-  events: []
+  events: [],
+  daysUntilWedding
 };
 
 export default (state = initialState, action) => {
