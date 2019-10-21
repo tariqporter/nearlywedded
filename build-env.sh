92f4ec65996866a38c16f5477c504f82e50d40da
Tariq <tic084@gmail.com>
2019-10-21 18:43:59 -0400

    travis test

--- build-env.sh
+++ build-env.sh
@@ -1,7 +1,17 @@
+FILE="./.env"
+
 if [[ $TRAVIS_BRANCH == 'production' ]]; then
-  REACT_APP_API_PATH=https://us-central1-nearlywedded-28143.cloudfunctions.net/api/
+cat <<EOM >$FILE
+REACT_APP_API_PATH=https://us-central1-nearlywedded-28143.cloudfunctions.net/api/
+EOM
+    echo "=========== $TRAVIS_BRANCH ENV ==========="
+    cat $FILE
 elif [[ $TRAVIS_BRANCH == 'master' ]]; then
-  REACT_APP_API_PATH=https://us-central1-staging-nearlywedded.cloudfunctions.net/api/
+cat <<EOM >$FILE
+REACT_APP_API_PATH=https://us-central1-staging-nearlywedded.cloudfunctions.net/api/
+EOM
+    echo "=========== $TRAVIS_BRANCH ENV ==========="
+    cat $FILE
 else
     exit 0
 fi
