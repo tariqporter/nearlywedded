d02a20ac2c3512ad3cdbc2ba70ab62ecbe26df56
Tariq <tic084@gmail.com>
2019-10-21 14:14:40 -0400

    travis test

--- functions/build-env.sh
+++
@@ -1,7 +0,0 @@
-if [[ $TRAVIS_BRANCH == 'production' ]]; then
-  REACT_APP_API_PATH=https://us-central1-nearlywedded-28143.cloudfunctions.net/api/
-elif [[ $TRAVIS_BRANCH == 'master' ]]; then
-  REACT_APP_API_PATH=https://us-central1-staging-nearlywedded.cloudfunctions.net/api/
-else
-    exit 0
-fi
