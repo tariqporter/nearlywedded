161fbe0201fb36b457e4448d5e5558a53b076b1c
Tariq <tic084@gmail.com>
2019-12-21 07:06:12 +0000

    run functions locally

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,5 @@
 const functions = require("firebase-functions");
-let admin = require('firebase-admin');
-const adminMock = require('./admin-mock');
+const admin = require('firebase-admin');
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
@@ -8,19 +7,8 @@ const expressip = require('express-ip');
 
 const app = express();
 app.use(expressip().getIpInfoMiddleware);
-let serviceAccount = functions.config().serviceaccount;
-if (serviceAccount) {
-  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
-}
-else {
-  serviceAccount = {};
-  admin = adminMock;
-}
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: serviceAccount.databaseurl
-});
+const adminApp = admin.initializeApp();
 const db = admin.firestore();
 
 // Automatically allow cross-origin requests
@@ -39,7 +27,7 @@ app.get('/data/user/:userId/', async (req, res) => {
   const ref = db.collection('users').doc(userId);
   const doc = await ref.get();
   if (!doc.exists) {
-    return res.json({ user: { id: null} });
+    return res.json({ user: { id: null } });
   }
   const data1 = doc.data();
   const user = { id: doc.id, name: data1.name };
@@ -47,27 +35,42 @@ app.get('/data/user/:userId/', async (req, res) => {
 });
 
 app.post('/data/user/saveTheDateViews/:userId/', async (req, res) => {
-  const { userId } = req.params;
-  const ref = db.collection('users').doc(userId);
-  const doc = await ref.get();
-  if (!doc.exists) {
-    return res.json({ saveDateViewDatesLength: -1 });
-  }
+  const defaultAuth = admin.auth();
+  console.log(defaultAuth);
+  // firebase.auth().signInWithEmailAndPassword('tic084@gmail.com', 'NearlyWedded!5248967041')
+  //   .then((firebaseUser) => {
+  //     console.log('success', firebaseUser.user.uid);
+  //     return firebaseUser;
+  //     // Success 
+  //   }).catch((error) => {
+  //     // Handle Errors here.
+  //     const errorCode = error.code;
+  //     const errorMessage = error.message;
+  //     console.log(errorCode, errorMessage);
+  //     // ...
+  //   });
+  // cyTPKaL9iVWfNDPYREDuW8IdmUV2
+  // const { userId } = req.params;
+  // const ref = db.collection('users').doc(userId);
+  // const doc = await ref.get();
+  // if (!doc.exists) {
+  //   return res.json({ saveDateViewDatesLength: -1 });
+  // }
 
-  const ipInfo = req.ipInfo;
-  const data1 = doc.data();
-  const saveDateViewDates = {
-    date: new Date(),
-    map: ipInfo && ipInfo.ll && ipInfo.ll.length >= 2 ? `http://www.google.com/maps/place/${ipInfo.ll[0]},${ipInfo.ll[1]}` : '',
-    info: ipInfo 
-  };
-  
-  ref.update({
-    saveDateViewDates: admin.firestore.FieldValue.arrayUnion(saveDateViewDates)
-  });
+  // const ipInfo = req.ipInfo;
+  // const data1 = doc.data();
+  // const saveDateViewDates = {
+  //   date: new Date(),
+  //   map: ipInfo && ipInfo.ll && ipInfo.ll.length >= 2 ? `http://www.google.com/maps/place/${ipInfo.ll[0]},${ipInfo.ll[1]}` : '',
+  //   info: ipInfo
+  // };
 
-  const saveDateViewDatesLength = !data1.saveDateViewDates ? 1 : data1.saveDateViewDates.length + 1;
-  return res.json({ saveDateViewDatesLength });
+  // ref.update({
+  //   saveDateViewDates: admin.firestore.FieldValue.arrayUnion(saveDateViewDates)
+  // });
+
+  // const saveDateViewDatesLength = !data1.saveDateViewDates ? 1 : data1.saveDateViewDates.length + 1;
+  return res.json({});
 });
 
 app.use(express.static(path.join(__dirname, 'build')));
