14c42b5f63460012df62b64d07f24efebbea8051
Tariq <tic084@gmail.com>
2020-01-05 16:45:49 -0500

    format main page

--- src/App.jsx
+++ src/App.jsx
@@ -14,37 +14,38 @@ const styles = theme => ({
     flexDirection: 'column',
     textAlign: 'center',
     alignItems: 'center',
-    height: '100%'
+    height: '100%',
   },
   body: {
     width: '100%',
     [theme.breakpoints.up('sm')]: {
-      width: 700
+      width: 700,
     },
   },
   img: {
-    background: 'url(/img/SLIDER-holly-hedge-wedding-country-romantic-fieldstone-barn-fountain-flowers-nighttime-beautiful1.jpg) 50% 50%',
+    background:
+      'url(/img/SLIDER-holly-hedge-wedding-country-romantic-fieldstone-barn-fountain-flowers-nighttime-beautiful1.jpg) 50% 50%',
     height: '300px',
-    backgroundSize: 'cover'
+    backgroundSize: 'cover',
   },
   link: {
     position: 'absolute',
     bottom: 5,
     right: 5,
-    color: '#fff'
+    color: '#fff',
   },
   countdown: {
-    fontWeight: 'bold'
-  }
+    fontWeight: 'bold',
+  },
 });
 
-const App = (props) => {
+const App = props => {
   const {
     classes = '',
     daysUntilWedding = null,
     location = {},
-    getUser = () => { },
-    getEvents = () => { }
+    getUser = () => {},
+    getEvents = () => {},
   } = props;
 
   useEffect(() => {
@@ -64,26 +65,39 @@ const App = (props) => {
       <div className={classes.body}>
         <div style={{ marginTop: 20, position: 'relative' }}>
           <div alt="Holly Hedge" className={classes.img} />
-          <a href="https://www.hollyhedge.com/" target="_blank" rel="noopener noreferrer" className={classes.link}>Holly Hedge Estate</a>
+          <a
+            href="https://www.hollyhedge.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={classes.link}
+          >
+            Holly Hedge Estate
+          </a>
         </div>
         <Events />
-        <div className={classes.countdown}>See you in {daysUntilWedding} days!</div>
+        <div className={classes.countdown}>
+          See you in {daysUntilWedding} days!
+        </div>
       </div>
     </div>
   );
-}
+};
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   const { daysUntilWedding } = state;
   return {
-    daysUntilWedding
-  }
-}
+    daysUntilWedding,
+  };
+};
 
-const mapDispatchToProps = (dispatch, ownProps) => bindActionCreators({
-  getEvents: getEventsAction,
-  getUser: getUserAction
-}, dispatch);
+const mapDispatchToProps = (dispatch, ownProps) =>
+  bindActionCreators(
+    {
+      getEvents: getEventsAction,
+      getUser: getUserAction,
+    },
+    dispatch
+  );
 
 export default connect(
   mapStateToProps,
